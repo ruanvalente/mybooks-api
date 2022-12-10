d20b5197e2195d12f4cd746bae3d9168768bf506
ruanvalente <ruanopensuselinux@gmail.com>
2022-12-10 20:07:04 -0300

    feat: list book by id

--- src/routes/books/index.ts
+++ src/routes/books/index.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 
 import { prisma } from '../../lib/prisma'
 
@@ -7,5 +8,31 @@ export async function booksRoutes(fastify: FastifyInstance) {
 		const books = await prisma.books.findMany()
 
 		return books
+	})
+
+	fastify.get('/books/:id', async (request, reply) => {
+		const bookId = z.object({
+			id: z.string(),
+		})
+
+		const { id } = bookId.parse(request.params)
+
+		const book = await prisma.books.findUnique({
+			where: {
+				id,
+			},
+		})
+
+		if (!id) {
+			return reply
+				.status(400)
+				.send({ message: 'You need to pass id of the book' })
+		}
+
+		if (!book) {
+			return reply.status(404).send({ message: 'Book not found' })
+		}
+
+		return book
 	})
 }
