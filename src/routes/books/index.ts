a35ec27bbc5728e9d82cd35db3f28aa57a0b3791
ruanvalente <ruanopensuselinux@gmail.com>
2022-12-10 19:55:13 -0300

    feat: list all books

--- src/routes/books/index.ts
+++ src/routes/books/index.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from 'fastify'
 
+import { prisma } from '../../lib/prisma'
+
 export async function booksRoutes(fastify: FastifyInstance) {
 	fastify.get('/books', async () => {
-		return [{}]
+		const books = await prisma.books.findMany()
+
+		return books
 	})
 }
