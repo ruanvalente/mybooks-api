3d3c201eb40b2098a78ddfcc86fc3cbd3a8c76b5
ruanvalente <ruanopensuselinux@gmail.com>
2022-12-10 19:35:33 -0300

    feat: add seed database

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function boostrap() {
+	const book = await prisma.books.create({
+		data: {
+			title:
+				'Orientação a Objetos Aprenda seus conceitos e suas aplicabilidades de forma efetiva',
+			status: 'READING',
+			imageURL:
+				'https://cdn.shopify.com/s/files/1/0155/7645/products/OrientacaoaObjetos_ebook_large.jpg?v=1631653034',
+			resume:
+				'Conforme a demanda por novos paradigmas que correspondessem às necessidades do dia a dia dos programadores, surgia a Orientação a Objetos, com a missão de cobrir as insuficiências do modelo estrutural. O Paradigma Orientado a Objeto tem como principal característica uma melhor e maior expressividade das nossas demandas e possibilita criar unidades de código mais próximas da forma como pensamos e agimos, facilitando o processo de transformação das necessidades diárias para uma linguagem orientada a objetos.',
+		},
+	})
+}
+
+boostrap()
